9101ef70da5b1cc94677956cb1bd0471f17f25ba
Isaac Duff <4mustangid@gmail.com>
2023-03-01 19:25:59 -0700

    Buttons Work Babbbbbbyyyy

--- app.js
+++ app.js
@@ -8,31 +8,37 @@ const iceCream = [{
 }, {
     name: 'Vanilla',
     image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ultimate-vanilla-ice-cream-1628511695.jpg',
-    price: 1
+    price: 1,
+    quantity: 0
 }, {
     name: 'Strawberry',
     image: 'https://www.realfoodwithjessica.com/wp-content/uploads/2017/07/paleostrawberryicecream2.jpg',
-    price: 2
+    price: 2,
+    quantity: 0
 }]
 
 const toppings = [{
     name: 'Sprinkles',
     image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Sprinkles2.jpg',
-    price: 1
+    price: 1,
+    quantity: 0
 }, {
     name: 'Chocolate Chips',
     image: 'https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/chocolate-chips.jpg?quality=82&strip=1&resize=640%2C360',
-    price: 2
+    price: 2,
+    quantity: 0
 }]
 
 const containers = [{
     name: 'Waffle Cone',
     image: 'https://m.media-amazon.com/images/I/71VNjBMakfL._SL1500_.jpg',
-    price: 2
+    price: 2,
+    quantity: 0
 }, {
     name: 'Waffle Bowl',
     image: 'http://images.wbmason.com/350/L_JOY66050.jpg',
-    price: 4
+    price: 4,
+    quantity: 0
 }]
 
 
@@ -60,7 +66,77 @@ function drawCart() {
     <h3>${c.quantity}</h3>
 </div>
 <div class="col-3">
-    <h3>${c.price}</h3>
+    <h3>$${c.price}</h3>
+</div>
+<div class="col-3">
+    <h3>Total</h3>
+</div>`}
+    })
+    document.getElementById('items').innerHTML = template
+
+
+}
+
+function addTopping(name) {
+    const top = toppings.find(t => t.name == name)
+
+    top.quantity++
+
+    console.log(top)
+
+
+    drawCartTop()
+}
+
+function drawCartTop() {
+    let total = 0
+    let template = ''
+    toppings.forEach(t => {
+        if (t.quantity) {
+            template += `
+    <div class="col-3">
+    <h3>${t.name}</h3>
+</div>
+<div class="col-3">
+    <h3>${t.quantity}</h3>
+</div>
+<div class="col-3">
+    <h3>$${t.price}</h3>
+</div>
+<div class="col-3">
+    <h3>Total</h3>
+</div>`}
+    })
+    document.getElementById('items').innerHTML = template
+
+
+}
+
+function addContainer(name) {
+    const con = containers.find(c => c.name == name)
+
+    con.quantity++
+
+    console.log(con)
+
+
+    drawCartCon()
+}
+
+function drawCartCon() {
+    let total = 0
+    let template = ''
+    containers.forEach(c => {
+        if (c.quantity) {
+            template += `
+    <div class="col-3">
+    <h3>${c.name}</h3>
+</div>
+<div class="col-3">
+    <h3>${c.quantity}</h3>
+</div>
+<div class="col-3">
+    <h3>$${c.price}</h3>
 </div>
 <div class="col-3">
     <h3>Total</h3>
